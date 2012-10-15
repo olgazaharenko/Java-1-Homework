52176f5a114a35425f1776300806dc8293633cad
Olga Zaharenko <olik1st@gmail.com>
2012-10-15 15:38:08 +0300

    userAnswer initialization corrected from 0 to 5.

--- src/OZA_JAVA1_Homework/MyTask5Class.java
+++ src/OZA_JAVA1_Homework/MyTask5Class.java
@@ -60,13 +60,13 @@ public class MyTask5Class {
 		
 		public static int askUserToCompareNumber (int lim) {
 			int userAnswer;
-			userAnswer=0;
+			userAnswer=5;
 			
-		/*    while (userAnswer != 0 || userAnswer != 1 || userAnswer != 2) { */
+			/* while (userAnswer != 0 || userAnswer != 1 || userAnswer != 2) { */ 
 		    	System.out.println ("Your number is less (0), more (1) or equal (2) then " + lim + "? Enter 0, 1 or 2.");
 		    	Scanner reader = new Scanner(System.in);
 		    	userAnswer = reader.nextInt();
-		/*    } */
+			/* } */
 		return userAnswer;
 		}
 		
