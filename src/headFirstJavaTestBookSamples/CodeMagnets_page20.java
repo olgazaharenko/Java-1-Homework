88abdd5fec7d2b1803708a27bb3586a4c6ca1082
Olga Zaharenko <olik1st@gmail.com>
2012-10-17 17:17:51 +0300

    JavaHeadFirst samples and exercises package.

---
+++ src/headFirstJavaTestBookSamples/CodeMagnets_page20.java
@@ -0,0 +1,34 @@
+package headFirstJavaTestBookSamples;
+
+public class CodeMagnets_page20 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int x=3;
+		
+		while (x>0) {
+		
+			if (x>2) {
+			System.out.print("a");
+			
+			}
+
+			x=x-1;
+			System.out.print("-");
+		
+			if (x==2) {
+			System.out.print("b c");
+			}
+		
+			if (x==1) {
+			System.out.print("d");
+			x=x-1;
+			}
+		
+		}
+		
+	}
+
+}
