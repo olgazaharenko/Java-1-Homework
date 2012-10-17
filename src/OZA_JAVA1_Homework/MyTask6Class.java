4d9200fafe9175199573e1c748af6a339f1256eb
Olga Zaharenko <olik1st@gmail.com>
2012-10-17 17:37:43 +0300

    try... catch construction

---
+++ src/OZA_JAVA1_Homework/MyTask6Class.java
@@ -0,0 +1,29 @@
+package OZA_JAVA1_Homework;
+
+public class MyTask6Class {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// try {
+		//
+	    // } 
+		// 
+		// catch () {
+		//
+	    // } 
+	    //      Here should be exception's handling inside the program,
+		// 		for example:
+		//     
+		// try {
+		//		readtheFile("myFile.txt");
+		// } 
+		// catch (FileNotFoundException ex) {
+		//      System.out.println("File Not Found.");
+		// }
+	
+	}
+
+}
