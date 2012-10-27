7ee46d68ea2b84c17c629f69809e111c7c077480
Olga Zaharenko <olik1st@gmail.com>
2012-10-27 18:12:22 +0300

    Algorithm's changes

--- src/OZA_JAVA1_Homework/KrestikiNoliki.java
+++ src/OZA_JAVA1_Homework/KrestikiNoliki.java
@@ -1,6 +1,7 @@
 package OZA_JAVA1_Homework;
 
 import java.util.Scanner;
+import java.awt.*;
 
 public class KrestikiNoliki {
 	
@@ -15,15 +16,24 @@ public class KrestikiNoliki {
     	return userAnswer;
 	}
 	
-	public void GameRules() {
+	public static void GameOver (){
+		
+		String gameOverMessage = "Game Over. Restart application to play again.";
+		
+		System.out.print (gameOverMessage);
+	}
+	
+	public static void GameRules() {
 		System.out.println("=============================================");
-		System.out.println("----------You will see 3x3 field:------------");
-		System.out.println("-----------------[ ] [ ] [ ] ----------------");
-		System.out.println("-----------------[ ] [ ] [ ] ----------------");
-		System.out.println("-----------------[ ] [ ] [ ] ----------------");
-		System.out.println("-------------Fill it either X or O.----------");
-		System.out.println("---Winner will first one from us, who will---");
-		System.out.println("---able to align your X or O in one line.----");
+		System.out.println("--------- You will see 3x3 field: -----------");
+		System.out.println("--------------=================--------------");
+		System.out.println("--------------== [ ] [ ] [ ] ==--------------");
+		System.out.println("--------------== [ ] [ ] [ ] ==--------------");
+		System.out.println("--------------== [ ] [ ] [ ] ==--------------");
+		System.out.println("--------------=================--------------");
+		System.out.println("---------- Fill it either X or O. -----------");
+		System.out.println("------- Winner will first one, who will -----");
+		System.out.println("--- able to align your X or O in one line. --");
 		System.out.println("=============================================");
 	}
 	
@@ -53,25 +63,43 @@ public class KrestikiNoliki {
 	}
 	
 	
-	public static String [][] CreateGameField () {
-		String [][] GameField = new String [3][3];
-		int i, j;
-		
-		for (i = 0; i<3; i++) {
-			for (j = 0; j<3; j++) {
-				GameField [i][j] = "_";
-				System.out.println (GameField [i][j]);
-			}			
-		}	
+	//public static void paint(Graphics g){
+		//draw a rectangle starting at 100,100 width a width and height of 80
+	//	  g.drawRect(100, 100, 80, 80);
+	//	}
 	
-		return GameField;		
+	public static void CreateGameField(String player1Name, String player2Name) {
+		
+		Graphics newGameField = null;
+		
+		System.out.println(player1Name + "X or O" + player2Name);
+		
+	//draw a rectangle starting at 100,100 width a width and height of 90
+		newGameField = g.drawRect(0, 0, 90, 90);
+		
+		
+		
+		//String [][] FilledGameField = new String [3][3];
+		
+		//String [][] GameField = new String [3][3];
+		//int i, j;
+		
+		//for (i = 0; i<3; i++) {
+		//	for (j = 0; j<3; j++) {
+		//		GameField [i][j] = "_";
+		//		System.out.println (GameField [i][j]);
+		//	}		
+		
+			
+		//}	
+	
 	}
 	
-	public static String [][] FillGameField () {
-		String [][] FilledGameField = new String [3][3];
+	public static void FillGameField () {
+	
 		// заполняем поле полученными ходами
 		// перерисовываем матрицу поля
-		return FilledGameField;		
+		
 	}
 	
 }
