472ff509f8ba43a08c23fc7c2b69d8b0590333f8
Olga Zaharenko <olik1st@gmail.com>
2012-10-30 15:18:35 +0200

    Исправления к задаче. main вызывает метод объекта Car, а уже внутри метода объекта Car происходит вызов метода объекта Light.

--- src/OZA_JAVA1_ClassWork/Car.java
+++ src/OZA_JAVA1_ClassWork/Car.java
@@ -2,21 +2,28 @@ package OZA_JAVA1_ClassWork;
 
 public class Car {
 	
-	private String trigger = "OFF";
-	private String lightOnOff;
-	private String inbetween = " Light is ";
-	
-	public String turnLight (Light myLight, String sideLight, boolean keyOnOff) {
+	public void buttonTurnMyLights(String q) {
 		
-	if (keyOnOff == false) {trigger = "ON";} else {trigger = "OFF";}
-	
-	lightOnOff = myLight.getLightSide(sideLight) + inbetween + trigger;
-	
-	return lightOnOff;
-	
+		//резервируем память под 4 фары
+		Light light[] = new Light[4];
+		
+		//помещаем туда ссылки на 4 фары
+		light[0] = new Light();
+		light[1] = new Light();
+		light[2] = new Light();
+		light[3] = new Light();
+		
+		//будем передавать номер фары, чтобы убедиться, что все 4 объекта отработали
+		String tmp;
+
+		//вызываем метод для включения либо выключения фар
+		for (int i = 0; i < 4; i++) {
+			tmp = Integer.toString(i+1);
+			light[i].turnLights(q, tmp);
+		}
 	}
-	
 }
+	
 
 
 
--- src/OZA_JAVA1_ClassWork/Light.java
+++ src/OZA_JAVA1_ClassWork/Light.java
@@ -2,31 +2,27 @@ package OZA_JAVA1_ClassWork;
 
 public class Light {
 
-	private String lFL = "FrontLeft";
-	private String lFR = "FrontRight";
-	private String lBL = "BackLeft"; 
-	private String lBR = "BackRight";
-	private String lightSide;
-	
-	public String getLightSide(String side) {
-		if (side == "FrontLeft") {
-			lightSide = lFL;
+	public void turnLights(String onOFF, String nameOfLight) {
+		
+		//создадим всякие вспомогательные тексты для отладки
+		String str1 = "===================";
+		String str2 = "Включаем фару " + nameOfLight;
+		String str3 = "Гасим фару ";
+		String str4 = " Light " + nameOfLight + " is " + onOFF;
+		
+		if (onOFF == "ON") {
+			//включаем фары
+			System.out.println(str1);
+			System.out.println(str2);
+			}
+		else {
+			//гасим фары
+			System.out.println(str1);
+			System.out.println(str3);
 		}
-		
-		if (side == "FrontRight") {
-			lightSide = lFR;
-		}
-		
-		if (side == "BackLeft") {
-			lightSide = lBL;
-		}
-		
-		if (side == "BackRight") {
-			lightSide = lBR;
-		}
-		
-		return lightSide;
-		
-	}
-		
+		System.out.println(str4);
+
+	}	
 }
+		
+
