91514d6d3cda27d28f4e50c1b5267333e23610a5
Olga Zaharenko <olik1st@gmail.com>
2012-11-17 18:11:55 +0200

    исправление метода setStringAsExpense класса ExpenseRegister

--- src/OZA_JAVA1_Homework/ExpenseRegister.java
+++ src/OZA_JAVA1_Homework/ExpenseRegister.java
@@ -49,8 +49,8 @@ public class ExpenseRegister {
 	     while(str.hasMoreTokens()){
 	    	 
 	    	 myStringAsExpense.setDate(str.nextToken());
-	    	 myStringAsExpense.setDate(str.nextToken());
-	    	 myStringAsExpense.setDate(str.nextToken());
+	    	 myStringAsExpense.setSum(str.nextToken());
+	    	 myStringAsExpense.setDescription(str.nextToken());
 	}
 		return myStringAsExpense;
 	}	
